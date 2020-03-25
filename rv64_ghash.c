412eab2a40b28d35ddfdc90469b5f9915381ef6a
Markku-Juhani O. Saarinen <mjos@iki.fi>
2020-03-25 09:23:24 +0000

    update

--- rv64_ghash.c
+++ rv64_ghash.c
@@ -25,7 +25,7 @@ void rv64_ghash_rev(gf128_t * z)
 void rv64_ghash_mul(gf128_t * z, const gf128_t * x, const gf128_t * h)
 {
 	uint64_t x0, x1, y0, y1;
-	uint64_t z0, z1, z2, z3, t0, t1;
+	uint64_t z0, z1, z2, z3, t0, t1, t2;
 
 	x0 = x->d[0];							//  new input
 	x1 = x->d[1];
@@ -42,60 +42,64 @@ void rv64_ghash_mul(gf128_t * z, const gf128_t * x, const gf128_t * h)
 	x0 = x0 ^ z0;							//  z is updated
 	x1 = x1 ^ z1;
 
-	//  Top and bottom, witha and w/o Karatsuba: 2 x CLMULHW, 2 x CLMULW
+#ifdef NO_KARATSUBA
+
+	(void) t2;								//  unused
+
+	//  Without Karatsuba; 4 x CLMULHW, 4 x CLMULW, 4 x XOR
+	z3 = rvb_clmulhw(x1, y1);
+	z2 = rvb_clmulw(x1, y1);
+	t1 = rvb_clmulhw(x0, y1);
+	z1 = rvb_clmulw(x0, y1);
+	z2 = z2 ^ t1;
+	t1 = rvb_clmulhw(x1, y0);
+	t0 = rvb_clmulw(x1, y0);
+	z2 = z2 ^ t1;
+	z1 = z1 ^ t0;
+	t1 = rvb_clmulhw(x0, y0);
+	z0 = rvb_clmulw(x0, y0);
+	z1 = z1 ^ t1;
+
+#else
+
+	//  With Karatsuba; 3 x CLMULHW, 3 x CLMULW, 8 x XOR
 	z3 = rvb_clmulhw(x1, y1);
 	z2 = rvb_clmulw(x1, y1);
 	z1 = rvb_clmulhw(x0, y0);
 	z0 = rvb_clmulw(x0, y0);
+	t0 = x0 ^ x1;
+	t2 = y0 ^ y1;
+	t1 = rvb_clmulhw(t0, t2);
+	t0 = rvb_clmulw(t0, t2);
+	t1 = t1 ^ z1 ^ z3;
+	t0 = t0 ^ z0 ^ z2;
+	z2 = z2 ^ t1;
+	z1 = z1 ^ t0;
 
-#ifdef NO_SHIFTRED
-	//  Mul reduction: 1 x CLMULHW, 1 x CLMULW, 1 x XOR
-	t1 = rvb_clmulhw(z3, 0x87);
-	t0 = rvb_clmulw(z3, 0x87);
-	t1 = t1 ^ z2;
-#else
-	//  Shift reduction: 6 x SHIFT, 6 x XOR 
-	t1 = (z3 >> 63) ^ (z3 >> 62) ^ (z3 >> 57) ^ z2;
-	t0 = z3 ^ (z3 << 1) ^ (z3 << 2) ^ (z3 << 7);
-#endif
-
-#ifdef NO_KARATSUBA
-	//  Without Karatsuba; 2 x CLMULHW, 2 x CLMULW, 4 * XOR
-	z3 = rvb_clmulhw(x0, y1);
-	z2 = rvb_clmulw(x0, y1);
-	z3 = z3 ^ t1;
-	z2 = z2 ^ t0;
-	t1 = rvb_clmulhw(x1, y0);
-	t0 = rvb_clmulw(x1, y0);
-	z3 = z3 ^ t1;
-	z2 = z2 ^ t0;
-#else
-	//  With Karatsuba; 1 x CLMULHW, 1 x CLMULW, 8 * XOR
-	z3 = z3 ^ t1 ^ z1;
-	z2 = z2 ^ t0 ^ z0;
-	x0 = x0 ^ x1;
-	x1 = y0 ^ y1;
-	t1 = rvb_clmulhw(x0, x1);
-	t0 = rvb_clmulw(x0, x1);
-	z3 = z3 ^ t1;
-	z2 = z2 ^ t0;
 #endif
 
 #ifdef NO_SHIFTRED
-	//  Mul reduction: 1 x CLMULHW, 1 x CLMULW, 1 x XOR
+
+	//  Mul reduction: 2 x CLMULHW, 2 x CLMULW, 4 x XOR
 	t1 = rvb_clmulhw(z3, 0x87);
 	t0 = rvb_clmulw(z3, 0x87);
-	t1 = t1 ^ z2;
+	z2 = z2 ^ t1;
+	z1 = z1 ^ t0;
+	t1 = rvb_clmulhw(z2, 0x87);
+	t0 = rvb_clmulw(z2, 0x87);
+	z1 = z1 ^ t1;
+	z0 = z0 ^ t0;
+
 #else
-	//  Shift reduction: 6 x SHIFT, 6 x XOR
-	t1 = (z3 >> 63) ^ (z3 >> 62) ^ (z3 >> 57) ^ z2;
-	t0 = z3 ^ (z3 << 1) ^ (z3 << 2) ^ (z3 << 7);
+
+	//  Shift reduction: 12 x SHIFT, 14 x XOR
+	z2 = z2 ^ (z3 >> 63) ^ (z3 >> 62) ^ (z3 >> 57);
+	z1 = z1 ^ z3 ^ (z3 << 1) ^ (z3 << 2) ^ (z3 << 7) ^
+		(z2 >> 63) ^ (z2 >> 62) ^ (z2 >> 57);
+	z0 = z0 ^ z2 ^ (z2 << 1) ^ (z2 << 2) ^ (z2 << 7);
+
 #endif
 
-	//  Low word; 2 x XOR
-	x1 = z1 ^ t1;
-	x0 = z0 ^ t0;
-
-	z->d[0] = x0;							//  inline to avoid these stores
-	z->d[1] = x1;
+	z->d[0] = z0;							//  inline to avoid these stores
+	z->d[1] = z1;
 }
